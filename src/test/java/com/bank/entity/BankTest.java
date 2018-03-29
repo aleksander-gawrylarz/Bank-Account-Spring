186512d8642042be887e55d118ca1e12c59e9c0a
aleksander1982 <aligawron@gmail.com>
2018-03-29 23:11:33 +0200

    Updated/Refactored

--- src/test/java/com/bank/entity/BankTest.java
+++ src/test/java/com/bank/entity/BankTest.java
@@ -1,51 +1,39 @@
 package com.bank.entity;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-import java.util.NoSuchElementException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import com.bank.form.LoginForm;
+import com.bank.BankAccountApplication;
+import com.bank.dao.BankDAO;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes=BankAccountApplication.class)
 public class BankTest {
 	
-	private Bank bank;
-	
-	private LoginForm loginForm = mock(LoginForm.class);
+	@Autowired
+	private BankDAO bankDAO;
 
 	@Before
 	public void setUp() throws Exception {
 		
-		bank = new Bank();
-		
-		bank.getClientList().add(new Client("John", "Doe", "johnD@gmail.com", new char[] {'j','o','h','n','7','5'}, 
+		bankDAO.getClients().add(new Client("John", "Doe", "johnD@gmail.com", new char[] {'j','o','h','n','7','5'}, 
 				new char[] {'p','a','s','s','7','5'}));
-		bank.getClientList().add(new Client("Marry", "Smith", "Marry@wp.pl", new char[] {'m','a','r','r'}, 
-				new char[] {'1','p','2','3'}));
+		
+		bankDAO.getLoginForm().setUsername(new char[] {'j','o','h','n','7','5'});
 	}
 	
 	@Test
 	public void whenGivenUsernameExistBankShouldReturnClientWithThatUsername() {
 		
-		Client expectedClient = bank.getClientList().get(0);
-		when(loginForm.getUsername()).thenReturn(new char[] {'j','o','h','n','7','5'});
+		Client expectedClient = bankDAO.getClients().get(0);
 		
-		assertEquals(expectedClient, bank.getClient(loginForm));
-		
-	}
-	
-	@Test (expected = NoSuchElementException.class)
-	public void whenGivenUsernameDoesNotExistNoSuchElementExceptionIsThrown() {
-		
-		Client expectedClient = bank.getClientList().get(1);
-		when(loginForm.getUsername()).thenReturn(new char[] {});
-		
-		assertEquals(expectedClient, bank.getClient(loginForm));
+		assertEquals(expectedClient, bankDAO.getClient());
 		
 	}
-
 }
